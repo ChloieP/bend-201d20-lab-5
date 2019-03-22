6dabade2423c547a335384b0904d7b325489e21b
ChloieP <chloieparsons@gmail.com>
2019-03-22 16:34:24 -0700

    sumAndMultiply in progress

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
   var theSum = a + b;
-  var theMessage = [theSum, 'The sum of ' + a + ' and ' + b + ' is ' + theSum +'.'];
-  return theMessage;
+  var theMessage = 'The sum of ' + a + ' and ' + b + ' is ' + theSum +'.';
+  return [theSum, theMessage];
 
 }
 
@@ -30,11 +30,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  var theProduct = a * b;
+  var theMessage = 'The product of ' + a + ' and ' + b + ' is ' + theProduct +'.';
+  return [theProduct, theMessage];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -51,11 +53,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  var initAdd = sum(a, b)[0];
+  var finAdd = sum(initAdd, c);
+  var initMult = multiply(a, b)[0];
+  var finMult = multiply(initMult, c);
+  return [finAdd, finMult, + a + ' and ' + b + ' and ' + c + ' sum to ' + finAdd + '.', 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + finMult +'.'];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
